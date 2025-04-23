1ade0888f0211ce8ba3c8d91f61e19f76e298b74
holger-at-annen <holger.hennrich@annen.eu>
2025-04-23 15:34:20 +0200

    Update 00-init-cron.sql

--- sql/00-init-cron.sql
+++ sql/00-init-cron.sql
@@ -1,6 +1,12 @@
--- Create extension in postgres database first
+-- Setup pg_cron in postgres database
 \c postgres
 CREATE EXTENSION IF NOT EXISTS pg_cron;
+CREATE EXTENSION IF NOT EXISTS dblink;
+
+-- Create schema and grant permissions
+CREATE SCHEMA IF NOT EXISTS cron;
+GRANT USAGE ON SCHEMA cron TO current_user;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA cron TO current_user;
 
 -- Switch to application database and create extension there
 \c ${POSTGRES_DB}
