1c9d48d31b6876b83de792d7e308c3fb18881ef4
holger-at-annen <holger.hennrich@annen.eu>
2025-04-25 08:28:18 +0200

    Update logging_config.py

--- app/logging_config.py
+++ app/logging_config.py
@@ -8,7 +8,7 @@ def setup_logging():
     
     # Create logs directory if it doesn't exist
     # log_dir = os.path.join(os.path.dirname(__file__), 'logs')
-    log_dir = 'app/logs'
+    log_dir = 'logs'
     os.makedirs(log_dir, exist_ok=True)
 
     # Configure the root logger
